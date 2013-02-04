9753b99b9ee0203fa8128655ce74cd5068e47f03
David Poole <testcluster@gmail.com>
2013-02-04 06:53:17 -0800

    working version! add zone2xml to write xml

--- slice_gtruth.py
+++ slice_gtruth.py
@@ -12,15 +12,19 @@ import sys
 import os
 import Image
 import ImageDraw
+import numpy as np
 
 import zonebox
 import rects
-from rects import Strip
 import drawboxes
+from basename import get_basename
+import mkslices
+import zone2xml
 
 strip_rows = 300
 
 def make_gtruth_slices( boxfilename ) : 
+    basename = get_basename( boxfilename )
 
     box_list = zonebox.load_boxes( boxfilename ) 
     print "found",len(box_list),"boxes"
@@ -37,10 +41,15 @@ def make_gtruth_slices( boxfilename ) :
     draw = ImageDraw.Draw(img)
     
     # starting strip as wide as the iamge with our base number of rows
-    s = Strip(width=num_cols, height=strip_rows )
+    s = rects.Strip(width=num_cols, height=strip_rows )
 
-    box_strip_list = [ Strip(box=box) for box in box_list ]
+    box_strip_list = [ rects.Strip(box=box) for box in box_list ]
 
+    data = np.asarray(img,dtype="uint8")
+    print "shape=",data.shape
+
+    # draw the ground truth in blue as sanity check (should see no blue in the
+    # output image)
     for box_strip in box_strip_list : 
         upper_left = box_strip.rect[0].x, box_strip.rect[0].y
         lower_right = box_strip.rect[2].x, box_strip.rect[2].y
@@ -50,6 +59,7 @@ def make_gtruth_slices( boxfilename ) :
     # iterate the strip down the page, calculating all the box intersections
     # for each strip
     row = 0
+    strip_counter = 0
     while row < num_rows : 
         print "strip=",s
 
@@ -57,6 +67,8 @@ def make_gtruth_slices( boxfilename ) :
         lower_right = s.rect[2].x, s.rect[2].y
         draw.rectangle( (upper_left,lower_right), outline="green" )
 
+        # linear search all the boxes searching for those that match this strip
+        box_intersect_list = []
         for box_strip in box_strip_list : 
             isect = rects.strip_intersect( box_strip, s )
             if isect : 
@@ -68,12 +80,24 @@ def make_gtruth_slices( boxfilename ) :
 
                 draw.rectangle( (upper_left,lower_right), outline="red" )
 
+                box_intersect_list.append( isect )
+
+        # save the intersections as XML
+        xmlfilename = "{0}_s{1}.xml".format( basename, strip_counter )
+        with open(xmlfilename,"w") as outfile :
+            zone2xml.write_boxlist_to_xml( outfile, box_intersect_list )
+        print "wrote", xmlfilename
+
         s.next_strip()
         row += strip_rows
+        strip_counter += 1
 
-    outfilename = "out.png"
+    outfilename = "{0}_out.png".format( basename )
     img.save(outfilename)
     print "wrote",outfilename
+
+    # write all the image slices
+#    mkslices.make_image_slices( imgfilename, strip_rows )
 
 def main() : 
     boxfilename = sys.argv[1]
