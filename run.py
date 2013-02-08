abeda94e0fbd29431f663315d4f7123efc406d8d
David Poole <testcluster@gmail.com>
2013-02-08 10:01:50 -0700

    deprecated by runseg.py which was a restart of the run a set of strip images through a rast test

--- run.py
+++ run.py
@@ -7,11 +7,14 @@ import sys
 import os
 import subprocess
 import glob
+import numpy as np
 
 from basename import get_basename
 import drawxml
 
-result_dir = "300_rast"
+num_rows_in_strip = 300
+seg_method = "rast"
+output_dir = None
 
 def parse_runZoneComp_result( strings ) :
 #    print strings
@@ -30,33 +33,64 @@ def parse_runZoneComp_result( strings ) :
     raise Exception( "Metric not found" )
 
 
-def get_stripnum_from_filename( name ) :
-    fields = name.split("_")
+#def get_stripnum_from_filename( name ) :
+#    fields = name.split("_")
+#
+#    stripnum = int(fields[1][1:])
+#    return stripnum
 
-    stripnum = int(fields[1][1:])
-    return stripnum
+def get_document_id_from_basename( basename ) :
+    # split the basename (e.g., ("A00BZONE_300_010_0000_rast") into just the
+    # UW-III document ID
+    fields = basename.split("_")
+    document_id = fields[0]
+    return document_id
+
+def make_output_dir( document_id ) : 
+    global output_dir
+    
+    # create an output directory from numrows, the segmentation method, and the
+    # UW-III document_id
+
+    output_dir = "{0}_{1}/{2}/".format( num_rows_in_strip, seg_method, document_id )
+    
+    if not os.path.exists(output_dir) :
+        os.mkdir( output_dir )
 
 def run( imgfilename ) : 
     basename = get_basename(imgfilename)
     
-    stripnum = get_stripnum_from_filename( basename )
+    document_id = get_document_id_from_basename( basename )
 
-    out_imgfilename = result_dir + "/" + "{0}_rast.png".format( basename )
-    xml_filename = result_dir + "/" + "{0}_rast.xml".format( basename ) 
+    # destination for the output files
+    make_output_dir(document_id) 
 
+#    stripnum = get_stripnum_from_filename( basename )
+
+    out_imgfilename = output_dir + "{0}_rast.png".format( basename )
+    xml_filename = output_dir + "{0}_rast.xml".format( basename ) 
+
+    input_dir = "{0}/{1}/".format( num_rows_in_strip, document_id )
+    
     # zone box files use "ZONE" instead of "BIN"
     # e.g., 
     # A00ABIN_300_010_2990.png -> A00AZONE_300_010_2990.xml 
-    gtruth_xml_filename = "300/{0}.xml".format( basename.replace("BIN","ZONE") )
+    gtruth_xml_filename = input_dir + "{0}.xml".format( basename.replace("BIN","ZONE") )
 
-    print imgfilename, out_imgfilename, xml_filename, gtruth_xml_filename
+    print "imgfilename=",imgfilename
+    print "out_imgfilename=",out_imgfilename
+    print "xml_filename=",xml_filename
+    print "gtruth_xml_filename=",gtruth_xml_filename
 #    sys.exit(0)
 
     # segment the image
     cmd = "./rast-ocropus {0} {1}".format( imgfilename, out_imgfilename ) 
     print cmd
 
-    result = subprocess.check_output( cmd, shell=True )
+    try : 
+        result = subprocess.check_output( cmd, shell=True )
+    except subprocess.CalledProcessError : 
+        return (imgfilename,"failed")
 
     # remove some clutter
     os.unlink(out_imgfilename)
@@ -77,7 +111,7 @@ def run( imgfilename ) :
     print "metric={0}".format( metric )
 
     # draw the experimental result onto the input image
-    out_imgfilename = result_dir + "/" + "{0}_rast_zone.png".format( basename )
+    out_imgfilename = output_dir + "{0}_rast_zone.png".format( basename )
     fname = drawxml.draw_zones( xml_filename, imgfilename, out_imgfilename )
     print "wrote", fname
 
@@ -86,30 +120,58 @@ def run( imgfilename ) :
 
     return (imgfilename,metric)
 
+def save_results( data ) : 
+    # get an output filename by parsing the first filename in the list
+    basename = get_basename( data[0][0] )
+    document_id = get_document_id_from_basename( basename ) 
+
+    print document_id
+
+    output_filename = output_dir + document_id + ".dat"
+
+    # get just the valid metrics
+    metric_list = []
+    num_failures = 0
+    for d in data :
+        try : 
+            metric = float(d[1])
+        except ValueError :
+            # skip
+            metric = 0
+            num_failures += 1
+        metric_list.append( metric )
+
+    print metric_list
+    metric_data = np.asarray( metric_list, dtype="float")
+    
+    outfile = open(output_filename,"w")
+
+    # write some overall statistics
+    print >>outfile, "# id={0}".format( document_id )
+    print >>outfile, "# mean={0}".format( np.mean(metric_data) )
+    print >>outfile, "# median={0}".format( np.median(metric_data) )
+    print >>outfile, "# stddev={0}".format( np.std(metric_data) )
+    print >>outfile, "# num_failures={0}".format( num_failures)
+
+    for d in data : 
+        basename = get_basename(d[0])
+        print >>outfile, basename,d[1]
+    outfile.close()
+
+#    with open(output_filename,"w") as outfile : 
+#        for d in data : 
+#            # print the metric
+#            print >>outfile, "{0}".format( d[1] )
+
 def main() : 
 
 #    for imgfilename in sys.argv[1:] : 
 #        run( imgfilename ) 
 
-    data = []
-
-    outfile = open("data.dat","w")
-
-    for f in sys.argv[1:] :
-        filename,metric = run(f)
-        print >>outfile, "{0}".format( metric )
-        outfile.flush()
-
-    outfile.close()
-    return
-
     data = [ run(f) for f in sys.argv[1:] ]
     print data
 
-    with open("data.dat","w") as outfile : 
-        for d in data : 
-            # print the metric
-            print >>outfile, "{0}".format( d[1] )
+    save_results( data ) 
 
 if __name__=='__main__' :
     main()
