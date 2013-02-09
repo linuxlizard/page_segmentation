544d4cc4c0ccc730c65acd7268c9e4ec1e9be68e
David Poole <testcluster@gmail.com>
2013-02-09 11:20:33 -0700

    add command line arguments to specify algorithm and files; add timer to kill hung jobs (voronoi, I'm looking at you)

--- runseg.py
+++ runseg.py
@@ -11,6 +11,8 @@ import os
 import pickle
 import glob
 import Image
+import argparse
+import signal
 
 from basename import get_basename 
 import drawxml
@@ -20,6 +22,12 @@ output_dir = "/tmp/"
 num_rows = 300
 uwiii_imgdir = "IMAGEBIN/"
 uwiii_xmldir = "ZONEXML/"
+
+#segmentation_cmd = "./rast-ocropus"
+#segmentation_algorithm = "rast"
+
+segmentation_cmd = "./voronoi-ocropus"
+segmentation_algorithm = "vor"
 
 def parse_runZoneComp_result( strings ) :
 #    print strings
@@ -45,12 +53,28 @@ def get_document_id_from_basename( basename ) :
     document_id = fields[0]
     return document_id
 
-def run( imgfilename, gtruth_xml_filename ) :
+running_filename = None
+
+def handler( signum, frame ) : 
+    print running_filename
+    result = subprocess.check_output( "ps waux | grep "+running_filename, shell=True )
+    print result
+
+    fields = result.split()
+    pid = int(fields[1])
+
+    print "killing pid=",pid 
+
+    os.kill(pid,signal.SIGKILL )
+
+def run_image_with_gtruth( imgfilename, gtruth_xml_filename ) :
     basename = get_basename(imgfilename)
 
     document_id = get_document_id_from_basename( basename )
-    out_imgfilename = output_dir + "{0}_rast.png".format( basename )
-    xml_filename = output_dir + "{0}_rast.xml".format( basename ) 
+    out_imgfilename = output_dir + "{0}_{1}.png".format( 
+                        basename, segmentation_algorithm )
+    xml_filename = output_dir + "{0}_{1}.xml".format( 
+                        basename, segmentation_algorithm ) 
 
     # inputs
 #    print "imgfilename=",imgfilename
@@ -62,17 +86,25 @@ def run( imgfilename, gtruth_xml_filename ) :
 #    sys.exit(0)
 
     # segment the image
-    cmd = "./rast-ocropus {0} {1}".format( imgfilename, out_imgfilename ) 
+    cmd = "{0} {1} {2}".format( segmentation_cmd, imgfilename, out_imgfilename ) 
+#    cmd = "./rast-ocropus {0} {1}".format( imgfilename, out_imgfilename ) 
 
+    signal.signal( signal.SIGALRM, handler )
     # small optimization; if the result already exists, don't run it again
     # (crash recovery)
+    print cmd
     if os.path.exists( xml_filename ) :
         print "{0} already exists so assume seg already run".format( xml_filename )
     else : 
-        print cmd
+        global running_filename 
+        running_filename = imgfilename 
+
+        signal.alarm( 5 )
         try : 
-            result = subprocess.check_output( cmd, shell=True )
+            result = subprocess.check_output( cmd.split() )
+            signal.alarm(0)
         except subprocess.CalledProcessError : 
+            signal.alarm(0)
             return { "output_image_file": out_imgfilename, 
                      "output_xml_file": "failed",
                      "metric" : 0 } 
@@ -83,10 +115,15 @@ def run( imgfilename, gtruth_xml_filename ) :
         print "wrote", xml_filename
 
     # run the compare
-    cmd = "runZoneComp -g {0} -d {1}".format( gtruth_xml_filename, xml_filename )
+    cmd = "./runZoneComp -g {0} -d {1}".format( gtruth_xml_filename, xml_filename )
     print cmd
 
-    result = subprocess.check_output( cmd, shell=True )
+    try : 
+        result = subprocess.check_output( cmd.split() )
+    except subprocess.CalledProcessError : 
+        return { "output_image_file": out_imgfilename, 
+                 "output_xml_file": "failed",
+                 "metric" : 0 } 
 
     # get the segmentation metric from the output
     metric = parse_runZoneComp_result( result ) 
@@ -113,7 +150,7 @@ def run_all_uwiii( ) :
 
 #        print basename, document_id, gtruth_xml_filename
 
-        result = run( imgfilename, gtruth_xml_filename )
+        result = run_image_with_gtruth( imgfilename, gtruth_xml_filename )
         result_list.append( result ) 
 
         # save pickled file so can do interesting things with the results later
@@ -158,7 +195,7 @@ def run_file_list( img_filelist, xml_filelist, output_basename ) :
         basename = get_basename(imgfilename)
         document_id = get_document_id_from_basename( basename )
 
-        result = run( imgfilename, gtruth_xml_filename )
+        result = run_image_with_gtruth( imgfilename, gtruth_xml_filename )
         result_list.append( result ) 
 
         # save pickled file so can do interesting things with the results later
@@ -193,30 +230,43 @@ def run_file_list( img_filelist, xml_filelist, output_basename ) :
             result["metric"] )
     outfile.close()
 
-def run_image( document_id ) : 
+def sanity_check_images( document_id ) : 
+    img_file_list,xml_file_list = get_file_lists( document_id )
+
+    for imgfilename,xmlfilename in zip(img_file_list,xml_file_list) : 
+        basename = get_basename(imgfilename)
+        document_id = get_document_id_from_basename( basename )
+
+        print imgfilename, xmlfilename, document_id
+
+        img = Image.open(imgfilename)
+        img.load()
+        xmlfile = open(xmlfilename,"r")
+        xmlfile.close()
+        del img
+
+def get_file_lists( document_id ) : 
+    # load all input images and their XML ground truth
     input_dir = str(num_rows) + "/" + document_id + "/"
 
     img_file_list = glob.glob( input_dir + "/*.png" ) 
 
     xml_file_list = [ s.replace(".png",".xml") for s in img_file_list ]
 
-    # sanity check before we begin the day's festivities
-    if 0 : 
-        for imgfilename in img_file_list : 
-            basename = get_basename(imgfilename)
-            xmlfilename = input_dir + basename + ".xml"
-            document_id = get_document_id_from_basename( basename )
+    return img_file_list, xml_file_list
 
-            print imgfilename, xmlfilename, document_id
+def run_image( document_id ) : 
 
-            img = Image.open(imgfilename)
-            img.load()
-            xmlfile = open(xmlfilename,"r")
-            xmlfile.close()
-            del img
+#    # sanity check before we begin the day's festivities
+#    sanity_check_images( document_id )
     
+    img_file_list,xml_file_list = get_file_lists( document_id )
+
     global output_dir
-    output_dir = str(num_rows) + "_rast/" + document_id + "/"
+
+    output_dir = "{0}_{1}/{2}/".format( 
+                    num_rows, segmentation_algorithm, document_id )
+
     if not os.path.exists(output_dir) :
         os.mkdir(output_dir)
 
@@ -235,15 +285,62 @@ def run_dir( dirname ) :
 
         print "{0} of {1}".format( count, len(img_dir_list) )
 
-def main() :
-    imgfilename = sys.argv[1]
-    xmlfilename = sys.argv[2]
+def parse_args() :
+    helptext = "Specify either docid OR image+gtruth"
 
-    run( imgfilename, xmlfilename ) 
+    parser = argparse.ArgumentParser( description="Run page segmentation" )
+
+    parser.add_argument( "--seg", help="segmentation algorithm to run (default==rast)",
+                         default="rast", choices=("rast","voronoi",) )
+    parser.add_argument( "--docid", help="document ID to segment" )
+    parser.add_argument( "--image", help="source image to segment" )
+    parser.add_argument( "--gtruth", help="ground truth XML" )
+
+    args = vars( parser.parse_args() )
+
+    print args
+
+    # sanity check some mutual exclusion arguments
+    if args["docid"] : 
+        if args["image"] or args["gtruth"] : 
+            err = "Error: must specify either docid OR image+gtruth\n"
+            parser.exit(-1,err)
+    else : 
+        if not ( args["image"] and args["gtruth"] ) : 
+            err = "Error: must specify both image and groundtruth files\n"
+            parser.exit(-1,err)
+
+    return args
+
+def main() :
+    args = parse_args() 
+
+    global segmentation_cmd
+    global segmentation_algorithm
+    if args["seg"]=="rast" : 
+        segmentation_cmd = "./rast-ocropus"
+        segmentation_algorithm = "rast"
+    elif args["seg"] == "voronoi" :
+        segmentation_cmd = "./voronoi-ocropus"
+        segmentation_algorithm = "vor"
+    else : 
+        raise Exception("Unknown segmentation algorithm "+args[seg] )
+        
+    if args["docid"] :
+        document_id = args["docid"]
+        run_image( document_id ) 
+
+    elif args["image"] : 
+        imgfilename = args["image"]
+        xmlfilename = args["gtruth"]
+
+        run_image_with_gtruth( imgfilename, xmlfilename ) 
 
 if __name__=='__main__':
 #    run_all_uwiii()
-#    main()
-#    run_image( "A005ZONE" )
-    run_dir("{0}/*".format(num_rows))
+    main()
+#    run_image( sys.argv[1] )
+#    sanity_check_images( sys.argv[1] )
+#    run_dir("{0}/*".format(num_rows))
 
+    
